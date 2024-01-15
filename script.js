7dd2a95139bd9821c91f4b43a1677ea815d11560
ARKID-THEGENIUS <143585115+ARKID-THEGENIUS@users.noreply.github.com>
2024-01-15 22:04:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+
+function white() {
+    document.body.style.backgroundColor = " white";
+    let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-4');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-4');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-4');
+    let elem4 = document.getElementById('na');
+    elem4.classList=[]
+    elem4.classList.add('navbar','nc-4');
+    let elem5 = document.getElementById('b-1');
+    elem5.classList=[]
+    elem5.classList.add('btn' );
+    let elem6 = document.getElementById('ab');
+    elem6.classList=[]
+    elem6.classList.add('about');
+    let elem7 = document.getElementById('co');
+    elem7.classList=[]
+    elem7.classList.add('contact' );
+    let elem8= document.getElementById('le');
+    elem8.classList=[]
+    elem8.classList.add('learn');
+    let elem9= document.getElementById('di');
+    elem9.classList=[]
+    elem9.classList.add('dive');
+    let elem10= document.getElementById('sh');
+    elem10.classList=[]
+    elem10.classList.add('searchbar');
+}
+function purple() {
+    document.body.style.backgroundColor = "#7049C3";
+    let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-1');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-1');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-1');
+    let elem4 = document.getElementById('na');
+    elem4.classList=[]
+    elem4.classList.add('navbar','nc-1');
+    let elem5 = document.getElementById('b-1');
+    elem5.classList=[]
+    elem5.classList.add('btn','nc-1' );
+    let elem6 = document.getElementById('ab');
+    elem6.classList=[]
+    elem6.classList.add('about','nc-1');
+    let elem7 = document.getElementById('co');
+    elem7.classList=[]
+    elem7.classList.add('contact','bgco' );
+    let elem8= document.getElementById('le');
+    elem8.classList=[]
+    elem8.classList.add('learn','wh');
+    let elem9= document.getElementById('di');
+    elem9.classList=[]
+    elem9.classList.add('dive','wh');
+    let elem10= document.getElementById('sh');
+    elem10.classList=[]
+    elem10.classList.add('searchbar','whi');
+    
+        }
+
+function blue() {
+    document.body.style.backgroundColor = "#0A2B9F";
+    let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-2');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-2');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-2');
+    let elem4 = document.getElementById('na');
+    elem4.classList=[]
+    elem4.classList.add('navbar','nc-2');
+    let elem5 = document.getElementById('b-1');
+    elem5.classList=[]
+    elem5.classList.add('btn','nc-2' );
+    let elem6 = document.getElementById('ab');
+    elem6.classList=[]
+    elem6.classList.add('about','nc-2');
+    let elem7 = document.getElementById('co');
+    elem7.classList=[]
+    elem7.classList.add('contact','bgco' );
+    let elem8= document.getElementById('le');
+    elem8.classList=[]
+    elem8.classList.add('learn','wh')
+    ;let elem9= document.getElementById('di');
+    elem9.classList=[]
+    elem9.classList.add('dive','wh');
+    let elem10= document.getElementById('sh');
+    elem10.classList=[]
+    elem10.classList.add('searchbar','whi');
+}
+function red() {
+    document.body.style.backgroundColor = "#B54242";
+    let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-3');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-3');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-3');
+    let elem4 = document.getElementById('na');
+    elem4.classList=[]
+    elem4.classList.add('navbar','nc-3');
+    let elem5 = document.getElementById('b-1');
+    elem5.classList=[]
+    elem5.classList.add('btn','nc-3' );
+    let elem6 = document.getElementById('ab');
+    elem6.classList=[]
+    elem6.classList.add('about','nc-3');
+    let elem7 = document.getElementById('co');
+    elem7.classList=[]
+    elem7.classList.add('contact','bgco' );
+    let elem8= document.getElementById('le');
+    elem8.classList=[]
+    elem8.classList.add('learn','wh');
+    let elem9= document.getElementById('di');
+    elem9.classList=[]
+    elem9.classList.add('dive','wh');
+    let elem10= document.getElementById('sh');
+    elem10.classList=[]
+    elem10.classList.add('searchbar','whi');
+}
